d4c2095cc55651cef9f588fc5260b23a888f487a
esantizooctagt <esantizo@octagt.com>
2021-12-14 20:20:55 -0600

    Revert "Revert "commit changes to app""
    This reverts commit b861014c61f2fb11e5d3c619ad62f86bda2a6fac.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        loadChildren: () => import('../comercio/comercio.module').then(m => m.ComercioPageModule)
+        loadChildren: '../comercio/comercio.module'
       },
       {
         path: '',
